ef088594958a1770ee729ffa6aa68194b575dc91
boram065 <boram4784@gmail.com>
2024-05-23 15:53:30 +0900

    models/get_absolute_url(); views CharacterDetailView; templates character_detail.html; character_list.html urls 로보카폴리:character_detail pk

--- NetworkProgramming_helloidol2/로보카폴리/models.py
+++ NetworkProgramming_helloidol2/로보카폴리/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.shortcuts import resolve_url
+
 
 # Create your models here.
 class Character(models.Model):
@@ -9,3 +11,6 @@ class Character(models.Model):
 
     def __str__(self): # 클래스를 출력할 때 이 함수를 호출하여 문자화한다.
         return f'이름: {self.name}, 특징: {self.feature}, 만든날짜: {self.created_at}, 수정한 날짜: {self.updated_at}'
+
+    def get_absolute_url(self): # 이 객체 하나의 데이터를 가져오는 url
+        return resolve_url('로보카폴리:detail', pk=self.id)
