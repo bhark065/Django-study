98ef71775036319856a21a99f9cc4bae4413f1d2
boram065 <boram4784@gmail.com>
2024-06-03 13:08:26 +0900

    2024-06-03

--- NetworkProgramming_240603_HouseholdAccountBook/chart_test/views.py
+++ NetworkProgramming_240603_HouseholdAccountBook/chart_test/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_chart(reqeust):
+    context = {}
+    return render(reqeust, 'chart_test/chart_view.html', context=context)
